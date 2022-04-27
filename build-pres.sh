0d480ad29acfe6b4a32f5ab0c47e3297042206a6
Joël BOURGAULT <joel.bourgault@kaizen-solutions.net>
2022-04-27 22:56:38 +0000

    clean(ci): removed useless volume in rendering

--- build-pres.sh
+++ build-pres.sh
@@ -2,7 +2,6 @@ sudo podman run \
 	-v `pwd`:/var/docs/:ro \
 	-v `pwd`/plantuml-images/:/var/docs/plantuml-images/:rw \
 	-v `pwd`/public/:/var/docs/public/:rw \
-	-v /tmp/:/tmp/:rw \
 	--entrypoint pandoc \
 	ojob/pandoc-plantuml \
 	--filter pandoc-plantuml \
